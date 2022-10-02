17420dd2407715f17e3a0f792731db1396d619bb
Jose <josee.nvm@gmail.com>
2022-10-02 02:41:33 +0200

    Changes to check victory algorithm

--- algoritmo.py
+++ algoritmo.py
@@ -32,6 +32,7 @@ def posiblesMovimientos(tablero):
             opciones += 1
     return opciones
 
+<<<<<<< Updated upstream
 # llama al algoritmo que decide la jugada
 def juega(tablero):
     profundidad = 3
@@ -44,6 +45,146 @@ def minimax(tablero, profundidad, jugador):
     fin = finArbol(tablero, profundidad)
     ganador = tablero.cuatroEnRaya()
     puntuacionMejor = 0
+=======
+def jugadaGanadora(tablero, fila, columna):
+    if ((fila is not None) and (columna is not None)):
+        ultimoJugador = tablero.getCelda(fila, columna)
+        if (combinacionHorizontal(tablero, fila, columna)):
+            return ultimoJugador
+        else:
+            if (combinacionVertical(tablero, fila, columna)):
+                return ultimoJugador
+            else:
+                if (combinacionDiagAsc(tablero, fila, columna)):
+                    return ultimoJugador
+                else:
+                    if (combinacionDiagDesc(tablero, fila, columna)):
+                        return ultimoJugador
+    return 0
+
+def combinacionHorizontal(tablero, fila, columna):
+    fichaBuscada = tablero.getCelda(fila, columna)
+    combinadas = 1
+    for columnaActual in range(columna + 1, columna + 4):
+        if (columnaActual < tablero.getAncho()):
+            fichaActual = tablero.getCelda(fila, columnaActual)
+            if (fichaActual == fichaBuscada):
+                combinadas += 1
+            else:
+                break
+        else:
+            break
+
+    if (combinadas == 4):
+        return True   
+
+    for columnaActual in range(columna - 1, columna - 4, -1):
+        if (columnaActual >= 0):
+            fichaActual = tablero.getCelda(fila, columnaActual)
+            if (fichaActual != fichaBuscada):
+                return False
+        else:
+            return False
+    return True
+    
+def combinacionVertical(tablero, fila, columna):
+    fichaBuscada = tablero.getCelda(fila, columna)
+    combinadas = 1
+    for filaActual in range(fila + 1, fila + 4):
+        if (filaActual < tablero.getAlto()):
+            fichaActual = tablero.getCelda(filaActual, columna)
+            if (fichaActual == fichaBuscada):
+                combinadas += 1
+            else:
+                break
+        else:
+            break
+
+    if (combinadas == 4):
+        return True   
+
+    for filaActual in range(fila - 1, fila - 4, -1):
+        if (filaActual >= 0):
+            fichaActual = tablero.getCelda(filaActual, columna)
+            if (fichaActual != fichaBuscada):
+                return False
+        else:
+            return False
+            
+    return True
+  
+def combinacionDiagAsc(tablero, fila, columna):
+    fichaBuscada = tablero.getCelda(fila, columna)
+    columnaActual = columna
+    combinadas = 1
+    for filaActual in range(fila - 1, fila - 4, -1):
+        columnaActual += 1
+        if ((filaActual >= 0) and (columnaActual < tablero.getAncho())):
+            fichaActual = tablero.getCelda(filaActual, columnaActual)
+            if (fichaActual == fichaBuscada):
+                combinadas += 1
+            else:
+                break
+        else:
+            break
+
+    if (combinadas == 4):
+        return True   
+
+    columnaActual = columna
+    for filaActual in range(fila + 1, fila + 4):
+        columnaActual -= 1
+        if ((filaActual < tablero.getAlto()) and (columnaActual >= 0)):
+            fichaActual = tablero.getCelda(filaActual, columnaActual)
+            if (fichaActual != fichaBuscada):
+                return False
+        else:
+            return False
+            
+    return True
+
+def combinacionDiagDesc(tablero, fila, columna):
+    fichaBuscada = tablero.getCelda(fila, columna)
+    columnaActual = columna
+    combinadas = 1
+    for filaActual in range(fila - 1, fila - 4, -1):
+        columnaActual -= 1
+        if ((filaActual >= 0) and (columnaActual >= 0)):
+            fichaActual = tablero.getCelda(filaActual, columnaActual)
+            if (fichaActual == fichaBuscada):
+                combinadas += 1
+            else:
+                break
+        else:
+            break
+
+    if (combinadas == 4):
+        return True   
+
+    columnaActual = columna
+    for filaActual in range(fila + 1, fila + 4):
+        columnaActual += 1
+        if ((filaActual < tablero.getAlto()) and (columnaActual < tablero.getAncho())):
+            fichaActual = tablero.getCelda(filaActual, columnaActual)
+            if (fichaActual != fichaBuscada):
+                return False
+        else:
+            return False
+            
+    return True
+
+# llama al algoritmo que decide la jugada y devuelve la posición elegida
+def juega(tablero):
+    profundidad = 5
+    mejorColumna = minimax(tablero, profundidad, False, None, None)[1]      
+    fila = busca(tablero, mejorColumna) 
+
+    return fila, mejorColumna
+    
+def minimax(tablero, profundidad, jugador, filaAnterior, columnaAnterior):
+    fin = finArbol(tablero, profundidad)
+    ganador = jugadaGanadora(tablero, filaAnterior, columnaAnterior)
+>>>>>>> Stashed changes
     mejorColumna = 0
     
     # gana alguien, empate o límite de  profunidad
@@ -79,16 +220,28 @@ def minimax(tablero, profundidad, jugador):
             # juega persona MIN
             if (jugador):
                 simulacionTablero.setCelda(fila, columna, 1)
+<<<<<<< Updated upstream
                 # recibe la mejor puntuación de las siguientes jugadas
                 puntuacionActual = minimax(simulacionTablero, profundidad-1, False)[0]
                 # si la puntuación es mejor que las anteriores se guarda la columna
+=======
+                # recibe la menor puntuación de las siguientes jugadas
+                puntuacionActual = minimax(simulacionTablero, profundidad-1, False, fila, columna)[0]
+                # si la puntuación es menor que las anteriores se guarda junto a la columna
+>>>>>>> Stashed changes
                 if (puntuacionActual < puntuacionMejor):
                     puntuacionMejor = puntuacionActual
                     mejorColumna = columna
             # juega máquina MAX
             else:
                 simulacionTablero.setCelda(fila, columna, 2)
+<<<<<<< Updated upstream
                 puntuacionActual = minimax(simulacionTablero, profundidad-1, True)[0]
+=======
+                # recibe la mayor puntuación de las siguientes jugadas
+                puntuacionActual = minimax(simulacionTablero, profundidad-1, True, fila, columna)[0]
+                # si la puntuación es mayor que las anteriores se guarda junto a la columna
+>>>>>>> Stashed changes
                 if (puntuacionActual > puntuacionMejor):
                     puntuacionMejor = puntuacionActual
                     mejorColumna = columna
